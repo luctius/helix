26cede5bd212ad24a205da7b534682b61a7653cb
Cor Peters <C.Peters@Eekels.com>
2023-04-04 08:45:04 +0200

    install: build grammars using hx; copy other runtime dirs seperatly

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
-git pull --rebase origin master
+git fetch
+git checkout personal_scripts && git pull luctius personal_scripts
+git checkout master && git pull origin master
+git checkout personal_scripts && git pull --rebase origin master && git push luctius personal_scripts -f
